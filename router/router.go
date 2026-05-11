e5031e71ed8b1d2b139fdeccba14afb130e8eb99
王磊 <wanglei9@edianyun.com>
2026-05-11 20:32:52 +0800

    router

--- router/router.go
+++ router/router.go
@@ -1,2 +1,50 @@
 package router
 
+
+import (
+	"fmt"
+	"time"
+	"github.com/gin-gonic/gin"
+	"NftAutionMarketBackend/controller"
+	"NftAutionMarketBackend/middleware"
+)
+
+
+
+func InitRouter() *gin.Engine{
+	r := gin.New()
+	r.Use(
+		//日志打印格式中间件
+		gin.LoggerWithFormatter(
+			func(param gin.LogFormatterParams) string  {
+				return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+				)
+			}
+		),
+		middleware.RecoveryHandler(),
+	)
+
+
+	//用户控制器
+	userController := controller.NewUserController()
+
+
+
+	authGroup := r.Group("/auth")
+	{
+
+	}
+
+}
+
+
+
